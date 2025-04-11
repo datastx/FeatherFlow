3e183ebf0333105d25dd92c6031c92f9783cc1c5
brian moore <datastx@gmail.com>
2025-04-11 17:12:42 +0000

    bm/better-feather-flow-local-exp

--- feather_flow/feather_flow.mk
+++ feather_flow/feather_flow.mk
@@ -1,2 +1,18 @@
 # Controls the build process of the Feather Flow library.
 
+# Version-related tasks
+.PHONY: version bump-version
+
+# Get the current version from Cargo.toml
+current-version:
+	@grep -m 1 "version" Cargo.toml | awk -F'"' '{print $$2}'
+
+# Bump version in Cargo.toml (usage: make bump-version NEW_VERSION=0.2.0)
+bump-version:
+	@echo "Current version: $$(grep -m 1 "version" Cargo.toml | awk -F'"' '{print $$2}')"
+	@sed -i 's/^version = "[0-9]*\.[0-9]*\.[0-9]*"/version = "$(NEW_VERSION)"/' Cargo.toml
+	@echo "Version bumped to $(NEW_VERSION)"
+	@echo "Don't forget to commit the changes and create a tag:"
+	@echo "git commit -am 'Bump version to $(NEW_VERSION)'"
+	@echo "git tag v$(NEW_VERSION)"
+	@echo "git push && git push --tags"
